b96c4e859160edc54d8d66569d5303dd69010960
shyunte <96578042+shyunte@users.noreply.github.com>
2022-03-25 23:56:02 +0900

    신현태

---
+++ src/main/java/boxOffice/dao/TheaterDao.java
@@ -0,0 +1,29 @@
+package boxOffice.dao;
+
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class TheaterDao {
+	private static TheaterDao instance = new TheaterDao();
+	private TheaterDao() {}
+	public static TheaterDao getInstance() {
+		return instance;
+	}
+	private static SqlSession session;
+	static {
+		try {
+			Reader reader = Resources.getResourceAsReader("configuration.xml");
+			SqlSessionFactory ssf = new SqlSessionFactoryBuilder().build(reader);
+			session = ssf.openSession(true);
+		}catch (Exception e) {
+			System.out.println("연결에러:"+e.getMessage());
+		}
+	}
+	
+	
+
+}
